e9a3acc6db67c29cd215f3f9797195899e11b99f
AttayR <attayrasoolofficial99@gmail.com>
2024-06-24 05:46:13 -0700

    fix customHeader Styling

---
+++ src/modules/VideoUpload/videoUpload1.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const videoUpload1 = () => {
+  return (
+    <View>
+      <Text>videoUpload1</Text>
+    </View>
+  )
+}
+
+export default videoUpload1
+
+const styles = StyleSheet.create({})
