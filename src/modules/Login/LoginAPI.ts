1672acce714558d88cda1e4618496097ac934248
Naveed Ahmad <naveed@livecosts.com>
2024-09-29 01:40:00 +0500

    Fix issues

--- src/modules/Login/LoginAPI.ts
+++ src/modules/Login/LoginAPI.ts
@@ -4,14 +4,14 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 
 export const LoginAPICall = async (payload: ILogin) => {
-    const response = await apiClient.post<ILoginResponse>(`login`, payload);
-    
-    if (response.data.data) {
-        await AsyncStorage.setItem('Token', response.data.data.token);
-      }
-      console.log("token:",response.data.data.token);
-      
-    return response.data;
+  const response = await apiClient.post<ILoginResponse>(`login`, payload);
+
+  if (response.data.data) {
+    await AsyncStorage.setItem('Token', response.data.data.token);
+    await AsyncStorage.setItem('profile', response.data.data.avatar);
+  }
+
+  return response.data;
 }
 
 export default LoginAPICall;
--- src/types/Profile.ts
+++ src/types/Profile.ts
@@ -1,13 +1,18 @@
 
 export interface IProfile {
-    id: number;
-    name: string;
-    email: string;
-    created_at: string;
-  }
-  
-  export interface IProfileResponse {
-    status: number;
-    data: IProfile[];
-  }
-  
+  id: number;
+  name: string;
+  email: string;
+  created_at: string;
+}
+
+export interface IProfileResponse {
+  status: number;
+  data: IProfile[];
+}
+
+export interface IUpdateAvatarResponse {
+  status: number;
+  avatar: string;
+  message: string;
+}
