103281539d403b574546e56e1b075563dfa9b2bf
AttayR <attayrasoolofficial99@gmail.com>
2024-10-12 15:11:08 +0100

    fix loop of animation in splash

--- src/modules/Splash/SplashView.tsx
+++ src/modules/Splash/SplashView.tsx
@@ -1,26 +1,26 @@
-// import React from 'react';
-// import { View, Text, ImageBackground, StatusBar } from 'react-native';
+// import React, { useEffect, useRef } from 'react';
+// import { View, StatusBar, StyleSheet, Dimensions } from 'react-native';
 // import AsyncStorage from '@react-native-async-storage/async-storage';
-// import styles from './styles';
-// import Button from '../../components/Button';
+// import LottieView from 'lottie-react-native';
 // import { useDispatch } from 'react-redux';
-// import { AppDispatch } from 'redux/store';
+// import { AppDispatch } from '../../redux/store';
 // import { fetchFirstLoginStatus } from '../../redux/Slices/FirstLogin';
 // import { useLoader } from '../../config/LoaderContext';
 
+// const { height, width } = Dimensions.get('window');
+
 // const SplashScreen = (props: any) => {
-//   const { route, navigation } = props
+//   const { navigation } = props;
 //   const dispatch = useDispatch<AppDispatch>();
-//   const { loading, setLoading } = useLoader();
+//   const { setLoading } = useLoader();
+//   const animationRef = useRef<LottieView>(null);
 
-
-//   const handleGetStarted = async () => {
+//   const handleNavigation = async () => {
 //     try {
 //       setLoading(true);
 //       const token = await AsyncStorage.getItem('Token');
 //       if (token) {
 //         const isFirstLogin = await dispatch(fetchFirstLoginStatus()).unwrap();
-
 //         if (isFirstLogin) {
 //           navigation.replace('Onboard');
 //         } else {
@@ -37,30 +37,40 @@
 //     }
 //   };
 
-//   return (
-//     <ImageBackground
-//       source={require('../../assets/Images/onBoarding.jpg')}
-//       style={styles.backgroundImage}
-//     >
-//       <StatusBar barStyle="light-content" translucent backgroundColor="transparent" />
-//       <View style={styles.container}>
-//         <Text style={styles.title}>Welcome to your Swing Coach!</Text>
-//         <Text style={styles.subtitle}>Get started with us and improve your game with personalized swing analysis.</Text>
-//         <Button
-//           title="Get Started"
-//           onPress={handleGetStarted}
-//           buttonStyle={styles.button}
-//           textStyle={styles.buttonText}
-//           disabled={loading}
-//           loading={loading}
-//         />
+//   useEffect(() => {
+//     if (animationRef.current) {
+//       animationRef.current.play();
+//     }
+//   }, []);
 
-//       </View>
-//     </ImageBackground>
+//   return (
+//     <View style={styles.container}>
+//       <StatusBar barStyle="light-content" translucent backgroundColor="transparent" />
+//       <LottieView
+//         ref={animationRef}
+//         source={require('../../assets/animations/Splash_loader.json')}
+//         autoPlay
+//         loop={false}
+//         style={styles.lottie}
+//         onAnimationFinish={handleNavigation} // Trigger navigation on animation finish
+//       />
+//     </View>
 //   );
 // };
 
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: 'white',
+//   },
+//   lottie: {
+//     width: width,
+//     height: height,
+//   },
+// });
+
 // export default SplashScreen;
+
 
 import React, { useEffect, useRef } from 'react';
 import { View, StatusBar, StyleSheet, Dimensions } from 'react-native';
@@ -98,6 +108,9 @@ const SplashScreen = (props: any) => {
       navigation.navigate('Login');
     } finally {
       setLoading(false);
+      if (animationRef.current) {
+        animationRef.current.pause(); 
+      }
     }
   };
 
@@ -105,6 +118,7 @@ const SplashScreen = (props: any) => {
     if (animationRef.current) {
       animationRef.current.play();
     }
+    handleNavigation();
   }, []);
 
   return (
@@ -114,9 +128,8 @@ const SplashScreen = (props: any) => {
         ref={animationRef}
         source={require('../../assets/animations/Splash_loader.json')}
         autoPlay
-        loop={false}
+        loop={true} 
         style={styles.lottie}
-        onAnimationFinish={handleNavigation} // Trigger navigation on animation finish
       />
     </View>
   );
@@ -134,4 +147,3 @@ const styles = StyleSheet.create({
 });
 
 export default SplashScreen;
-
